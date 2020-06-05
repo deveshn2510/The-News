09721d1d16b322b47719e0195f00b70154928358
devesh chaudhary <deveshn2510@gmail.com>
2020-06-05 13:58:12 +0530

    Search Added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,51 +1,59 @@
 import React, { useEffect, useState } from 'react';
 
 import Style from './style.module.css';
+import Search from '../search/search';
 
 const Header = () => {
-    const [long, setLong] = useState('');
-    const [lat, setLat] = useState('');
+    const [long, setLong] = useState(null);
+    const [lat, setLat] = useState(null);
     const [temp, setTemp] = useState('');
+    const [location, setLocation] = useState('');
+    const [tempDes, setTempDes] = useState('');
+
 
     useEffect(() => {
-       const getLocations=async function() 
-       {
-        await navigator.geolocation.getCurrentPosition(function (position) {
-            console.log("Latitude is :", position.coords.latitude);
-            console.log("Longitude is :", position.coords.longitude);
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+            // console.log("Latitude is :", position.coords.latitude);
+            // console.log("Longitude is :", position.coords.longitude);
             setLat(position.coords.latitude);
             setLong(position.coords.longitude);
         })
-       } 
-        
-        const fetchData = async function() 
-        {
-         await  fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=a6ca8aac3488196c4d79075708e299f9`)
-        .then(res => res.json())
-        .then(res => { 
-            const data = res.wind.speed;
-            console.log(data)
-            setTemp(data)
-        })
-        }
-
-        getLocations();
-        fetchData();
-        
-
-        
+        fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=a6ca8aac3488196c4d79075708e299f9`)
+            .then(res => res.json())
+            .then(res => {
+                const data = res;
+                // console.log(data)
+                setTemp(data.main.temp);
+                setTempDes(data.weather[0].main);
+                setLocation(data.name);
+            })
     }, [lat, long])
+
+
+
     return (
         <div>
             <div className={Style.container}>
-                <div className={Style.header}>
-                    The News
+                <div className={Style.innerContanier}>
+                    <div className={Style.header}>
+                        The News
+                    </div>
+                    <div className={Style.tempDetails}>
+                        {`${(temp - 273.15).toPrecision(3)} °C`}
+
+                        <span className={Style.span}>{`, ${tempDes}`}</span>
+                        <div>
+                            {location}
+                        </div>
+                    </div>
+                
                 </div>
-                <div className={Style.temp}>
-                  {temp-273.15}
-                </div>
+                
 
             </div>
+            <Search />
+            
         </div>
     )
 }
---
+++ src/components/search/search.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import Style from './style.module.css'
+
+const Search = () => {
+    const [ dropdownValue , setDropdownValue] = useState('en');
+    const [ searchValue , setSearchValue] = useState('');
+
+    
+const buttonPressed=()=>{
+    console.log(searchValue , dropdownValue);
+    if(searchValue==''){
+     //   https://gnews.io/api/v3/top-news?token=API-Token
+    }
+    // https://gnews.io/api/v3/search?q=mobile&lang=fr&token=ec280f1563e12b361b37c0e32fc6d7e4
+}
+
+
+    return (
+        <div>
+            <div className={Style.container}>
+                <input className={Style.input} placeholder='Search..' type='search' value={searchValue} onChange={e=> setSearchValue(e.target.value)}></input>
+                <select className={Style.select} value={dropdownValue} onChange={(e) => { setDropdownValue(e.target.value) }} >
+                    <option value='af'>Afrikaans</option>
+                    <option value='sq'>Albanian</option>
+                    <option value='sm'>Amharic</option>
+                    <option value='az'>Azerbaijani</option>
+                    <option value='eu'>Basque</option>
+                    <option value='be'>Belarusian</option>
+                    <option value='bn'>Bengali</option>
+                    <option value='bh'>Bihari</option>
+                    <option value='bs'>Bosnian</option>
+                    <option value='bg'>Bulgarian</option>
+                    <option value='ca'>Catalan</option>
+                    <option value='hr'>Croatian</option>
+                    <option value='cs'>Czech</option>
+                    <option value='da'>Danish</option>
+                    <option value='nl'>Dutch</option>
+                    <option value='en'>English</option>
+                    <option value='eo'>Esperanto</option>
+                    <option value='et'>Estonian</option>
+                    <option value='fo'>Faroese</option>
+                    <option value='fi'>Finnish</option>
+                    <option value='fr'>French</option>
+                    <option value='fy'>Frisian</option>
+                    <option value='gl'>Galician</option>
+                    <option value='ka'>Georgian</option>
+                    <option value='de'>German</option>
+                    <option value='el'>Greek</option>
+                    <option value='gu'>Gujarati</option>
+                    <option value='iw'>Hebrew</option>
+                    <option value='hi'>Hindi</option>
+                    <option value='hu'>Hungarian</option>
+                    <option value='is'>Icelandic</option>
+                    <option value='id'>Indonesian</option>
+                    <option value='ia'>Interlingua</option>
+                    <option value='ga'>Irish</option>
+                    <option value='it'>Italian</option>
+                    <option value='ja'>Japanese</option>
+                    <option value='jw'>Javanese</option>
+                    <option value='kn'>Kannada</option>
+                    <option value='ko'>Korean</option>
+                    <option value='la'>Latin</option>
+                    <option value='lv'>Latvian</option>
+                    <option value='lt'>Lithuanian</option>
+                    <option value='mk'>Macedonian</option>
+                    <option value='ms'>Malay</option>
+                    <option value='ml'>Malayam</option>
+                    <option value='mt'>Maltese</option>
+                    <option value='mr'>Marathi</option>
+                    <option value='ne'>Nepali</option>
+                    <option value='no'>Norwegian</option>
+                    <option value='oc'>Occitan</option>
+                    <option value='fa'>Persian</option>
+                    <option value='pl'>Polish</option>
+                    <option value='pa'>Punjabi</option>
+                    <option value='ro'>Romanian</option>
+                    <option value='ru'>Russian</option>
+                    <option value='gd'>Scots Gaelic</option>
+                    <option value='sr'>Serbian</option>
+                    <option value='si'>Sinhalese</option>
+                    <option value='sk'>Slovak</option>
+                    <option value='sl'>Slovenian</option>
+                    <option value='es'>Spanish</option>
+                    <option value='su'>Sudanese</option>
+                    <option value='sw'>Swahili</option>
+                    <option value='sv'>Swedish</option>
+                    <option value='tl'>Tagalog</option>
+                    <option value='ta'>Tamil</option>
+                    <option value='te'>Telugu</option>
+                    <option value='th'>Thai</option>
+                    <option value='ti'>Tigrinya</option>
+                    <option value='tr'>Turkish</option>
+                    <option value='uk'>Ukrainian</option>
+                    <option value='ur'>Urdu</option>
+                    <option value='uz'>Uzbek</option>
+                    <option value='vi'>Vietnamese</option>
+                    <option value='cy'>Welsh</option>
+                    <option value='xh'>Xhosa</option>
+                    <option value='zu'>Zulu</option>
+
+
+
+                </select>
+                <button className={Style.button} onClick={buttonPressed}>Search</button>
+            </div>
+            <div className={Style.normal}>
+                created
+            </div>
+        </div>
+    )
+}
+
+export default Search;
+
